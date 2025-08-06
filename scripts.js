0d14b0d7b5081a6c138eb28d316fa2d7d2ebf9fc
Josué Soares <josuejj.24@proton.me>
2025-08-06 15:47:45 -0300

    Feature de reiniciar via pulo

--- scripts.js
+++ scripts.js
@@ -1,39 +1,45 @@
 const dino = document.getElementById("dino");
 const cactosTipo = ['cacto0', 'cacto1', 'cacto2'];
 
+let vivo = true;
+let cacto;
+
+document.addEventListener("keydown", function (event) {
+    if(event.code === "ArrowUp" || event.code === "Space"){
+            if(vivo){
+                pulo();
+            }else{
+                reiniciar();
+            }
+        }
+    });
 
 function escolhaCacto(){
-    //sorteia um tipo de cacto para ser gerado
-    let cactoId = Math.floor(Math.random()*cactosTipo.length);
-    let cactoEscolhido = cactosTipo[cactoId];
-    let cacto = document.getElementById(`${cactoEscolhido}`);
-    cacto.style.backgroundImage = `url('${cactoEscolhido}.png')`;
+        //sorteia um tipo de cacto para ser gerado
+        let cactoId = Math.floor(Math.random()*cactosTipo.length);
+        let cactoEscolhido = cactosTipo[cactoId];
+        cacto = document.getElementById(`${cactoEscolhido}`);
+        cacto.style.backgroundImage = `url('${cactoEscolhido}.png')`;
 
-    //Detectção de hit
-    let estado = setInterval(function(){
-        let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-        let cactoleft = parseInt(window.getComputedStyle(cacto).getPropertyValue("left"));
+        cacto.classList.remove("cacto");
+        void cacto.offsetHeight;
+        cacto.classList.add("cacto"); 
 
-        if(cactoleft<50 && cactoleft>0 && dinotop>120){
-            alert("Perdeu");
-        }
-    }, 10);
+        detecção_de_hit(cacto);
 
-    //Serve para chamar a função novamente após 1 segundo, que é o que dura a animação
-    setTimeout(function(){
-       cacto.style.backgroundImage = "none";
-
-       escolhaCacto();
-    }, 1000);
-}
-
-escolhaCacto();
-
-
-function pulo(){
-    if(dino.classList!="pulo"){
-        dino.classList.add("pulo");
+        //Serve para chamar a função novamente após 1 segundo, que é o que dura a animação
+        setTimeout(function(){
+            if(vivo){
+                cacto.style.backgroundImage = "none";
+                escolhaCacto();
+            }
         
+        }, 1000);
+    }
+function pulo(){
+    if(dino.classList!="pulo" && vivo){
+        dino.classList.add("pulo");
+           
         // Remove a classe pulo após a animação
         setTimeout(function(){
             dino.classList.remove("pulo");
@@ -41,9 +47,27 @@ function pulo(){
     }
 }
 
+function reiniciar(){
+    dino.style.backgroundImage = 'url("dino0.png")';
+    vivo = true;
+    cacto.style.backgroundImage = "none";
+    cacto.style.animationPlayState = "running";
+    escolhaCacto();
+}
 
-document.addEventListener("keydown", function (event) {
-  if(event.code === "ArrowUp" || event.code === "Space"){
-    pulo();
-  }
-});
+function detecção_de_hit(cacto){
+    let estado = setInterval(function(){
+        let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        let cactoleft = parseInt(window.getComputedStyle(cacto).getPropertyValue("left"));
+        
+        if(cactoleft<50 && cactoleft>0 && dinotop>120){
+            vivo = false;
+            dino.style.backgroundImage = 'url("dino-dead0.png")';
+            cacto.style.animationPlayState = "paused";
+            clearInterval(estado);
+        }
+    }, 10);
+}
+
+//Inicia o Jogo
+escolhaCacto();
